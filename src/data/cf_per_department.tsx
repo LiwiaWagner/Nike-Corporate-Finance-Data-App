183662693fd010c7a3b3dabbdc5e3d0495a57e9b
LiwiaWagner <liwia.wagner@gmail.com>
2026-01-02 09:21:22 +0100

    cf_per_dept data added

---
+++ src/data/cf_per_department.tsx
@@ -0,0 +1,73 @@
+export const cfDepartment: {
+  fiscal_year: number
+  "Cash provided (used) by operations": number
+  "Cash provided (used) by investing activities": number
+  "Cash provided (used) by financing activities": number
+}[] = [
+  {
+    fiscal_year: 2015,
+    "Cash provided (used) by operations": 4680000,
+    "Cash provided (used) by investing activities": -175000,
+    "Cash provided (used) by financing activities": -2790000,
+  },
+  {
+    fiscal_year: 2016,
+    "Cash provided (used) by operations": 3399000,
+    "Cash provided (used) by investing activities": -1034000,
+    "Cash provided (used) by financing activities": -2974000,
+  },
+  {
+    fiscal_year: 2017,
+    "Cash provided (used) by operations": 3846000,
+    "Cash provided (used) by investing activities": -1008000,
+    "Cash provided (used) by financing activities": -2148000,
+  },
+  {
+    fiscal_year: 2018,
+    "Cash provided (used) by operations": 4955000,
+    "Cash provided (used) by investing activities": 276000,
+    "Cash provided (used) by financing activities": -4835000,
+  },
+  {
+    fiscal_year: 2019,
+    "Cash provided (used) by operations": 5903000,
+    "Cash provided (used) by investing activities": -264000,
+    "Cash provided (used) by financing activities": -5293000,
+  },
+  {
+    fiscal_year: 2020,
+    "Cash provided (used) by operations": 2485000,
+    "Cash provided (used) by investing activities": -1028000,
+    "Cash provided (used) by financing activities": 2491000,
+  },
+  {
+    fiscal_year: 2021,
+    "Cash provided (used) by operations": 6657000,
+    "Cash provided (used) by investing activities": -3800000,
+    "Cash provided (used) by financing activities": -1459000,
+  },
+  {
+    fiscal_year: 2022,
+    "Cash provided (used) by operations": 5188000,
+    "Cash provided (used) by investing activities": -1524000,
+    "Cash provided (used) by financing activities": -4836000,
+  },
+  {
+    fiscal_year: 2023,
+    "Cash provided (used) by operations": 5841000,
+    "Cash provided (used) by investing activities": 564000,
+    "Cash provided (used) by financing activities": -7447000,
+  },
+  {
+    fiscal_year: 2024,
+    "Cash provided (used) by operations": 7429000,
+    "Cash provided (used) by investing activities": 894000,
+    "Cash provided (used) by financing activities": -5888000,
+  },
+  {
+    fiscal_year: 2025,
+    "Cash provided (used) by operations": 3698000,
+    "Cash provided (used) by investing activities": -275000,
+    "Cash provided (used) by financing activities": -5820000,
+  },
+]
